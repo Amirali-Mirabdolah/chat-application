ecc3480e73a24420c779bcf991ddaa97ac7102d6
amirali mirabdolah <amiralimirabdolah@gmail.com>
2026-07-20 15:07:41 +0330

    feat(user-requests): use requests in UI

--- app/(root)/friends/page.tsx
+++ app/(root)/friends/page.tsx
@@ -1,18 +1,49 @@
-import ConversationFallback from '@/components/shared/conversation/ConversationFallback'
-import ItemList from '@/components/shared/item-list/ItemList'
-import AddFriendDialog from './_components/AddFriendDialog'
+"use client";
 
-type Props = {}
+import ConversationFallback from "@/components/shared/conversation/ConversationFallback";
+import ItemList from "@/components/shared/item-list/ItemList";
+import AddFriendDialog from "./_components/AddFriendDialog";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Loader2 } from "lucide-react";
+import Request from "./_components/Request";
+
+type Props = {};
 
 const FriendsPage = (props: Props) => {
+  const requests = useQuery(api.requests.get);
+
   return (
     <>
-      <ItemList title='FriendsPage' action={<AddFriendDialog />}>
-        FriendsPage
+      <ItemList
+        title="Friends"
+        action={<AddFriendDialog />}
+      >
+        {requests ? (
+          requests.length === 0 ? (
+            <p className="w-full h-full flex items-center justify-center">
+              no friend request found
+            </p>
+          ) : (
+            requests.map((request) => {
+              return (
+                <Request
+                  key={request.request._id}
+                  id={request.request._id}
+                  imageUrl={request.sender.imageUrl}
+                  userName={request.sender.username}
+                  email={request.sender.email}
+                />
+              );
+            })
+          )
+        ) : (
+          <Loader2 className="size-8" />
+        )}
       </ItemList>
       <ConversationFallback />
     </>
-  )
-}
+  );
+};
 
-export default FriendsPage
+export default FriendsPage;
--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -11,6 +11,7 @@
 import type * as _utils from "../_utils.js";
 import type * as http from "../http.js";
 import type * as request from "../request.js";
+import type * as requests from "../requests.js";
 import type * as user from "../user.js";
 
 import type {
@@ -23,6 +24,7 @@ declare const fullApi: ApiFromModules<{
   _utils: typeof _utils;
   http: typeof http;
   request: typeof request;
+  requests: typeof requests;
   user: typeof user;
 }>;
 
