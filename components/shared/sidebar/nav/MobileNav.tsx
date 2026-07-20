046c8b157a5736a86d6df9fb08ab2efc8b1e0a39
amirali mirabdolah <amiralimirabdolah@gmail.com>
2026-07-20 17:59:48 +0330

    feat(badge): add badge for aware of user request count

--- components/shared/sidebar/nav/MobileNav.tsx
+++ components/shared/sidebar/nav/MobileNav.tsx
@@ -1,56 +1,68 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { Card } from "@/components/ui/card"
-import { ThemeToggle } from "@/components/ui/theme/theme-toggle"
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
-import { useConversation } from "@/hooks/useConversation"
-import { useNavigation } from "@/hooks/useNavigation"
-import { UserButton } from "@clerk/nextjs"
-import Link from "next/link"
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { ThemeToggle } from "@/components/ui/theme/theme-toggle";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useConversation } from "@/hooks/useConversation";
+import { useNavigation } from "@/hooks/useNavigation";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 const MobileNav = () => {
+  const paths = useNavigation();
+  const { isActive } = useConversation();
 
-    const paths = useNavigation()
-    const { isActive } = useConversation()
+  if (isActive) return null;
 
-    if (isActive) return null
+  return (
+    <Card className="lg:hidden fixed bottom-4 w-[calc(100vw-32px)] flex items-center h-16 p-2">
+      <nav className="w-full h-full">
+        <ul className="h-full flex justify-evenly items-center">
+          {paths.map((path, id) => {
+            return (
+              <li
+                className="relative"
+                key={id}
+              >
+                <Link href={path.href}>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <Button
+                        size="icon"
+                        variant={path.active ? "default" : "outline"}
+                      >
+                        {path.icon}
+                      </Button>
+                      {path.count ? (
+                        <Badge className="absolute left-7 bottom-6 px-2">
+                          {path.count}
+                        </Badge>
+                      ) : null}
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>{path.name}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </Link>
+              </li>
+            );
+          })}
+          <li>
+            <UserButton />
+          </li>
+          <li>
+            <ThemeToggle />
+          </li>
+        </ul>
+      </nav>
+    </Card>
+  );
+};
 
-    return (
-        <Card className="lg:hidden fixed bottom-4 w-[calc(100vw-32px)] flex items-center h-16 p-2">
-            <nav className="w-full h-full">
-                <ul className="h-full flex justify-evenly items-center">
-                    {paths.map((path, id) => {
-                        return (
-                            <li className="relative" key={id}>
-                                <Link href={path.href}>
-                                    <Tooltip>
-                                        <TooltipTrigger>
-                                            <Button
-                                                size="icon"
-                                                variant={path.active ? "default" : "outline"}
-                                            >
-                                                {path.icon}
-                                            </Button>
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p>{path.name}</p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </Link>
-                            </li>
-                        )
-                    })}
-                    <li>
-                        <UserButton />
-                    </li>
-                    <li>
-                        <ThemeToggle />
-                    </li>
-                </ul>
-            </nav>
-        </Card>
-    )
-}
-
-export default MobileNav
+export default MobileNav;
--- hooks/useNavigation.tsx
+++ hooks/useNavigation.tsx
@@ -1,24 +1,31 @@
-import { MessageSquare, Users } from "lucide-react"
-import { usePathname } from "next/navigation"
-import { useMemo } from "react"
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { MessageSquare, Users } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
 
 export const useNavigation = () => {
-    const pathname = usePathname()
+  const pathname = usePathname();
+  const requestsCount = useQuery(api.requests.count);
 
-    const paths = useMemo(() => [
-        {
-            name: "Conversations",
-            href: "/conversations",
-            icon: <MessageSquare />,
-            active: pathname.startsWith("/conversations")
-        },
-        {
-            name: "Friends",
-            href: "/friends",
-            icon: <Users />,
-            active: pathname === "/friends"
-        }
-    ], [pathname])
+  const paths = useMemo(
+    () => [
+      {
+        name: "Conversations",
+        href: "/conversations",
+        icon: <MessageSquare />,
+        active: pathname.startsWith("/conversations"),
+      },
+      {
+        name: "Friends",
+        href: "/friends",
+        icon: <Users />,
+        active: pathname === "/friends",
+        count: requestsCount,
+      },
+    ],
+    [pathname, requestsCount],
+  );
 
-    return paths
-}
+  return paths;
+};
